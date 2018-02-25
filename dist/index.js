f44ffa5f1f3b0ceb3788926757e3c7d00bee4698
LachlanArthur <lachlanarthur@gmail.com>
2018-02-25 17:33:13 +1000

    Finish the thing

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,65 @@
 import { $, on } from './helpers.js';
+// Braille symbol is 2x4 dots
+const asciiXDots = 2, asciiYDots = 4;
+let threshold = 127, asciiWidth = 100, asciiHeight;
 on(document, 'DOMContentLoaded', function (e) {
-    on($('#filepicker'), 'change', function (e) {
-        console.log(this.files);
+    on($('#filepicker'), 'change', render);
+    on($('#threshold'), 'change', function () {
+        let newValue = parseInt(this.value);
+        if (newValue == threshold)
+            return;
+        threshold = newValue;
+        render();
+    });
+    on($('#width'), 'blur', function () {
+        let newValue = parseInt(this.value);
+        if (newValue == asciiWidth)
+            return;
+        asciiWidth = newValue;
+        render();
     });
 });
+async function render() {
+    let input = $('#filepicker');
+    let ascii = '';
+    if (!input.files || !input.files.length)
+        return;
+    let image = document.createElement('img');
+    image.src = URL.createObjectURL(input.files[0].slice(0));
+    await new Promise(resolve => on(image, 'load', resolve));
+    asciiHeight = Math.ceil(asciiWidth * asciiXDots * (image.height / image.width) / asciiYDots);
+    let canvas = document.createElement('canvas');
+    let context = canvas.getContext('2d');
+    canvas.width = asciiWidth * asciiXDots;
+    canvas.height = asciiHeight * asciiYDots;
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    for (let y = 0; y < canvas.height; y += asciiYDots) {
+        for (let x = 0; x < canvas.width; x += asciiXDots) {
+            ascii += ImageData2Braille(context.getImageData(x, y, asciiXDots, asciiYDots));
+        }
+        ascii += '<br>';
+    }
+    let output = $('#output');
+    output.style.display = 'block';
+    output.innerHTML = ascii;
+}
+function ImageData2Braille(data) {
+    if (data.width != asciiXDots || data.height != asciiYDots)
+        throw TypeError(`Braille image data must be ${asciiXDots}px wide and ${asciiYDots}px high.`);
+    let dots = [];
+    for (let i = 0; i < asciiXDots * asciiYDots; i++) {
+        dots[i] = data.data.subarray(i * 4, (i + 1) * 4);
+    }
+    // Reorder dots to match Braille dot order
+    dots = [
+        dots[0], dots[3],
+        dots[1], dots[4],
+        dots[2], dots[5],
+        dots[6], dots[7],
+    ];
+    dots = dots
+        .map(([r, g, b, a]) => (r + g + b) / 3)
+        .map(grey => +(grey < threshold));
+    return String.fromCharCode(10240 + parseInt(dots.reverse().join(''), 2));
+}
 //# sourceMappingURL=index.js.map
